b0081b6af2263929d95ae16ef7a1ba8aac5c3955
Ragnazar <ivan.avdeyev@wilix.org>
2022-07-31 14:41:58 +0300

    commit-add-tasks-6-and-7

--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,26 @@ public class Main {
         task3();
         task4();
         task5();
+        task6();
+        task7();
 
     }
 
     private static void task1() {
         byte clientOS = 1;
-        if (clientOS == 0){
+        if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        }   else if (clientOS == 1) System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 1) System.out.println("Установите версию приложения для Android по ссылке");
     }
 
     private static void task2() {
         byte clientOS = 1;
         int year = 2014;
-        if (clientOS == 0 && year >= 2015 && year < 2023){
+        if (clientOS == 0 && year >= 2015 && year < 2023) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        }   else if (clientOS == 1 && year >= 2015 && year < 2023){
+        } else if (clientOS == 1 && year >= 2015 && year < 2023) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        }
-        else if (clientOS == 0 && year < 2015) {
+        } else if (clientOS == 0 && year < 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         } else if (clientOS == 1 && year < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
@@ -33,7 +34,7 @@ public class Main {
 
     private static void task3() {
         int year = 2021;
-        if (year % 4 != 0 || (year % 100 == 0 && year % 400 != 0)){
+        if (year % 4 != 0 || (year % 100 == 0 && year % 400 != 0)) {
             System.out.println(year + " год не является високосным");
         } else {
             System.out.println(year + " год является високосным");
@@ -78,5 +79,47 @@ public class Main {
             default:
                 System.out.println("Месяца с номером " + monthNumber + " не существует");
         }
+    }
+
+    private static void task6() {
+        byte age = 19;
+        int salary = 58000;
+        byte ageLimit = 23;
+        int salaryLimitLow = 50000;
+        int salaryLimitHigh = 80000;
+        if (age >= ageLimit && salary >= salaryLimitLow) {
+            System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + (3 * salary * 1.2) + " рублей.");
+        } else if (age >= ageLimit && salary >= salaryLimitHigh) {
+            System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + (3 * salary * 1.5) + " рублей.");
+        } else if (age < ageLimit && salary >= salaryLimitLow) {
+            System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + (2 * salary * 1.2) + "рублей.");
+        } else if (age < ageLimit && salary >= salaryLimitHigh) {
+            System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + (2 * salary * 1.5) + "рублей.");
+        } else if (age < 18) {
+            System.out.println("Извините, мы не готовы выдать вам кредитную карту");
+        }
+    }
+
+    private static void task7() {
+        byte age = 27;
+        int salary = 20000;
+        int wantedSum = 330000;
+        byte baseTerm = 12;
+
+        double maxMonthlyPay = 0.5 * salary;
+        double averageMonthPay = Math.ceil(wantedSum * 1.1 / baseTerm);
+
+        if (age < 23 && salary >= 80000 && (averageMonthPay * 1.01 * (1 - 0.007)) <= maxMonthlyPay) {
+            System.out.println("Максимальный платеж при ЗП " + salary + " равен " + maxMonthlyPay + " рублей. Платеж по кредиту " + Math.ceil(averageMonthPay * 1.01 * (1 - 0.007)) + " рублей. Одобрено».");
+        } else if (age < 23 && salary < 80000 && (averageMonthPay * 1.01) <= maxMonthlyPay) {
+            System.out.println("Максимальный платеж при ЗП " + salary + " равен " + maxMonthlyPay + " рублей. Платеж по кредиту " + Math.ceil(averageMonthPay * 1.01) + " рублей. Одобрено».");
+        } else if (age < 30 && salary >= 80000 && (averageMonthPay * 1.005 * (1 - 0.007)) <= maxMonthlyPay) {
+            System.out.println("Максимальный платеж при ЗП " + salary + " равен " + maxMonthlyPay + " рублей. Платеж по кредиту " + Math.ceil(averageMonthPay * 1.005 * (1 - 0.007)) + " рублей. Одобрено».");
+        } else if (age < 30 && salary < 80000 && (averageMonthPay * 1.005) <= maxMonthlyPay) {
+            System.out.println("Максимальный платеж при ЗП " + salary + " равен " + maxMonthlyPay + " рублей. Платеж по кредиту " + Math.ceil(averageMonthPay * 1.005) + " рублей. Одобрено».");
+        } else {
+            System.out.println("Максимальный платеж при ЗП " + salary + " равен " + maxMonthlyPay + " рублей. Платеж по кредиту " + averageMonthPay + " рублей. Неодобрено.");
+        }
+
     }
 }
